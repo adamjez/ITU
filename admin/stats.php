adea9cb3e29fc9a4a8ea4853d852ad6b9372f024
Roman Blanco <roman.blanco92@gmail.com>
2014-11-23 21:22:13 +0100

    upravene udaje v grafech

--- admin/stats.php
+++ admin/stats.php
@@ -1,21 +1,21 @@
 <div class="col-xs-12 col-sm-6 col-md-8">
-  <h2>Weekly visits</h2>
+  <h2>Monthly visits</h2>
   <div class="panel panel-default">
     <div class="panel-body">
-      <canvas id="weekly" height="40" width="100" style="padding-right: 30px;"></canvas>
+      <canvas id="monthly" height="40" width="100" style="padding-right: 30px;"></canvas>
       <script>
-        var weeklyData = {
+        var monthlyData = {
             labels: ["January", "February", "March", "April", "May", "June", "July"],
             datasets: [
                 {
-                    label: "My First dataset",
+                    label: "",
                     fillColor: "rgba(220,220,220,0.2)",
                     strokeColor: "rgba(220,220,220,1)",
                     pointColor: "rgba(220,220,220,1)",
                     pointStrokeColor: "#fff",
                     pointHighlightFill: "#fff",
                     pointHighlightStroke: "rgba(220,220,220,1)",
-                    data: [65, 59, 80, 81, 56, 55, 40]
+                    data: [1265, 1259, 1280, 2281, 2156, 2295, 1322]
                 }
             ]
 
@@ -23,7 +23,7 @@
       </script>
     </div>
   </div>
-  <h2>Users platform </h2>
+  <h2>Users platform & Browsers</h2>
   <div class="panel panel-default">
     <div class="panel-body">
       <div class="canvas-holder-half">
@@ -34,19 +34,43 @@
                   value: 300,
                   color:"#F7464A",
                   highlight: "#FF5A5E",
-                  label: "Windows"
+                  label: "Windows 7"
               },
               {
-                  value: 50,
+                  value: 400,
+                  color:"#F7464A",
+                  highlight: "#FF5A5E",
+                  label: "Windows 8"
+              },
+              {
+                  value: 2,
+                  color:"#F7464A",
+                  highlight: "#FF5A5E",
+                  label: "Windows 10"
+              },
+              {
+                  value: 500,
                   color: "#46BFBD",
                   highlight: "#5AD3D1",
                   label: "Android"
               },
               {
-                  value: 100,
-                  color: "#FDB45C",
+                  value: 50,
+                  color: "#CCAABB",
+                  highlight: "#CCBBBB",
+                  label: "Mac OS X"
+              },
+              {
+                  value: 20,
+                  color: "#FFBF70",
                   highlight: "#FFC870",
                   label: "Linux"
+              },
+              {
+                  value: 50,
+                  color: "#112288",
+                  highlight: "#1122BB",
+                  label: "BlackBerry"
               }
           ]
         </script>
@@ -56,31 +80,46 @@
         <script>
           var browserData = [
               {
-                  value: 300,
+                  value: 377,
                   color:"#F7464A",
                   highlight: "#FF5A5E",
-                  label: "Windows"
+                  label: "Firefox"
               },
               {
-                  value: 50,
-                  color: "#46BFBD",
-                  highlight: "#5AD3D1",
-                  label: "Android"
+                  value: 503,
+                  color: "#652770",
+                  highlight: "#82478C",
+                  label: "Google Chrome"
               },
               {
-                  value: 100,
+                  value: 323,
+                  color: "#4E9231",
+                  highlight: "#76B75B",
+                  label: "Opera"
+              },
+              {
+                  value: 22,
+                  color: "#5864D9",
+                  highlight: "#7B85E6",
+                  label: "Maxthon"
+              },
+              {
+                  value: 97,
                   color: "#FDB45C",
                   highlight: "#FFC870",
-                  label: "Linux"
+                  label: "Internet Explorer"
               }
           ]
         </script>
       </div>
       <script>
         window.onload = function(){
-            var ctx_line = document.getElementById("weekly").getContext("2d");
-            window.myLine = new Chart(ctx_line).Line(weeklyData, {
-                responsive: true
+            var ctx_line = document.getElementById("monthly").getContext("2d");
+            window.myLine = new Chart(ctx_line).Line(monthlyData, {
+                responsive: true,
+                bezierCurve: false,
+                pointDot: true,
+                datasetFill: true,
             });
             var ctx_pie_i = document.getElementById("platform").getContext("2d");
             window.myPie = new Chart(ctx_pie_i).Pie(platformData, {
@@ -100,24 +139,29 @@
       <h3>Refering sites</h3>
       <table class="table table-bordered">
         <tr>
-          <td>Site</td>
-          <td>Views</td>
-          <td>Unique visitors</td>
+          <td><strong>Site</strong></td>
+          <td><strong>Views</strong></td>
+            <td><strong>Unique visitors</strong></td>
         </tr>
         <tr>
           <td>google.cz</td>
-          <td>40</td>
-          <td>7</td>
+          <td>501</td>
+          <td>407</td>
         </tr>
         <tr>
           <td>idnes.cz</td>
-          <td>400</td>
-          <td>70</td>
+          <td>452</td>
+          <td>370</td>
         </tr>
         <tr>
           <td>vutbr.cz</td>
-          <td>4000</td>
-          <td>700</td>
+          <td>327</td>
+          <td>224</td>
+        </tr>
+        <tr>
+          <td>seznam.cz</td>
+          <td>42</td>
+          <td>39</td>
         </tr>
       </table>
       <h3>Popular content</h3>
@@ -128,19 +172,19 @@
           <td>Unique visitors</td>
         </tr>
         <tr>
-          <td>index.html</td>
-          <td>40</td>
-          <td>7</td>
+          <td>school-stuff.html</td>
+          <td>559</td>
+          <td>470</td>
         </tr>
         <tr>
-          <td>school-stuff.html</td>
-          <td>400</td>
-          <td>70</td>
+          <td>index.html</td>
+          <td>442</td>
+          <td>321</td>
         </tr>
         <tr>
           <td>idontknow.html</td>
-          <td>4000</td>
-          <td>700</td>
+          <td>321</td>
+          <td>249</td>
         </tr>
       </table>
     </div>
